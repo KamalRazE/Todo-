bb646569712c9fd35ed102a109f472fe2d1f2089
KamalRazE <pi314sa@gmail.com>
2025-08-12 22:37:01 +0530

    Update index.js
    const PORT = process.env.PORT || 3000;
    app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`);
    });

--- index.js
+++ index.js
@@ -61,4 +61,7 @@ app.get("/", (req, res) => {
     res.render("list", { tasks: filteredTasks, filter });
 });
 
-app.listen(8000, () => console.log("Server running on port 8000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
